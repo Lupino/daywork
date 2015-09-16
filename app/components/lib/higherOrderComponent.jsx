509acbce46aaf4dcee107004d909f5abefe534bd
Lupino <lmjubuntu@gmail.com>
2015-09-16 14:38:39 +0800

    update higherOrderComponent

--- app/components/lib/higherOrderComponent.jsx
+++ app/components/lib/higherOrderComponent.jsx
@@ -4,15 +4,33 @@ export function alert(Component) {
   return class extends React.Component {
     state = {
       modal: false,
+      callback: null,
       msg: ''
     }
 
-    toggleModal(type, msg) {
-      this.setState({ modal: type, msg: msg });
+    toggleModal(type) {
+      this.setState({ modal: type });
     }
 
-    alert(msg) {
-      this.toggleModal('alert', msg);
+    alert(msg, callback) {
+      this.setState({ modal: 'alert', msg, callback });
+    }
+
+    confirm(msg, callback) {
+      this.setState({ modal: 'confirm', msg, callback });
+    }
+
+    onConfirm() {
+      if (this.state.callback) {
+        this.state.callback(true);
+      }
+    }
+
+    onClose() {
+      if (this.state.callback && this.state.modal === 'alert') {
+        this.state.callback(false);
+      }
+      this.toggleModal(false);
     }
 
     render() {
@@ -21,8 +39,9 @@ export function alert(Component) {
           {this.state.modal && <Modal
             title="提示"
             type={this.state.modal}
-            onClose={this.toggleModal.bind(this, false)}>{this.state.msg} </Modal>}
-          <Component {...this.props} alert={this.alert} />;
+            onConfirm={this.onConfirm}
+            onClose={this.onClose}>{this.state.msg} </Modal>}
+          <Component {...this.props} alert={this.alert} confirm={this.confirm} />;
         </div>
       );
     }
