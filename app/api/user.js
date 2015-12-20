54f848dc9adcdb8c48d7e3f4146322159a52f764
Lupino <lmjubuntu@gmail.com>
2015-12-20 13:48:29 +0800

    Add default userId

--- app/api/user.js
+++ app/api/user.js
@@ -1,25 +1,42 @@
 import request from './request';
 import { getUri, wapperCallback } from './utils';
+import store from '../modules/store';
 
 export function getProfile(callback) {
   request.get(getUri('/api/users/me'), wapperCallback(callback));
 }
 
 export function getUser({ userId }, callback) {
+  if (!userId) {
+    const profile = store.get('profile');
+    userId = profile.userId;
+  }
   request.get(getUri(`/api/users/${userId}`), wapperCallback(callback));
 }
 
-export function getUserJobs({ userId, page, limit }, callback) {
-  request.get(getUri(`/api/users/${userId}/jobs`, {limit, page}),
+export function getUserJobs({ userId, status, page, limit }, callback) {
+  if (!userId) {
+    const profile = store.get('profile');
+    userId = profile.userId;
+  }
+  request.get(getUri(`/api/users/${userId}/jobs`, {status, limit, page}),
               wapperCallback(callback));
 }
 
 export function getUserWorks({ userId, page, limit }, callback) {
+  if (!userId) {
+    const profile = store.get('profile');
+    userId = profile.userId;
+  }
   request.get(getUri(`/api/users/${userId}/works`, { limit, page}),
               wapperCallback(callback));
 }
 
 export function getUserWork({ userId, jobId }, callback) {
+  if (!userId) {
+    const profile = store.get('profile');
+    userId = profile.userId;
+  }
   request.get(getUri(`/api/users/${userId}/works/${jobId}`),
               wapperCallback(callback));
 }
