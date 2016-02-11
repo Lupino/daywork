1f402f2d3afe4aabb476a263830463b0b28e9bf2
Lupino <lmjubuntu@gmail.com>
2016-02-11 20:54:18 +0800

    add dev mode

--- src/lib/leancloud.js
+++ src/lib/leancloud.js
@@ -1,16 +1,22 @@
 import AV from 'leanengine';
-import { leancloud } from '../config';
+import { leancloud, dev } from '../config';
 
 const { appId, appKey, masterKey } = leancloud;
 
 AV.initialize(appId, appKey, masterKey);
 
 export function requestSmsCode(phoneNumber, callback) {
+  if (dev) {
+    return callback();
+  }
   AV.Cloud.requestSmsCode(phoneNumber).then(()=>callback(),
                                             (err) => callback(err));
 }
 
 export function verifySmsCode(code, phoneNumber, callback) {
+  if (dev) {
+    return callback();
+  }
   AV.Cloud.verifySmsCode(code, phoneNumber).then(()=>callback(),
                                                  (err) => callback(err));
 }
