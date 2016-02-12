78c6b220160f18bf4d3ee226f13e347d54e9edc4
Lupino <lmjubuntu@gmail.com>
2016-02-12 22:07:05 +0800

    Add JobInfo actions

--- app/jobs/JobInfo.jsx
+++ app/jobs/JobInfo.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import { ProgressBar } from 'react-toolbox';
-import { getJob } from '../api';
+import { ProgressBar, CardActions, IconButton } from 'react-toolbox';
+import { getJob, favorite, unfavorite, requestJob } from '../api';
 import JobItem from './JobItem';
 import style from '../style';
 import lodash from 'lodash';
@@ -25,6 +25,48 @@ export default class JobInfo extends Component {
     });
   };
 
+  handleFavorite(fav) {
+    const { notify } = this.props;
+    const { jobId } = this.state.job;
+    let method = fav ? favorite : unfavorite;
+    this.updateFavorite(fav);
+    method({ jobId }, (err) => {
+      if (err) {
+        this.updateFavorite(!fav);
+        return notify(err);
+      }
+    });
+  }
+
+  updateFavorite(fav) {
+    let { job } = this.state;
+    job.favorited = fav;
+    this.setState({ job });
+  }
+
+  handleShowPhoneNumber(phoneNumber) {
+    const { alert } = this.props;
+    alert({ message: phoneNumber, title: '电话号码' });
+  }
+
+  handleRequestJob() {
+    const { notify } = this.props;
+    const { jobId } = this.state.job;
+    this.updateRequest(true);
+    requestJob({ jobId }, (err) => {
+      if (err) {
+        this.updateRequest(false);
+        return notify(err);
+      }
+    });
+  }
+
+  updateRequest(req) {
+    let { job } = this.state;
+    job.requested = req;
+    this.setState({ job });
+  }
+
   componentDidMount() {
     this.loadJob();
   }
@@ -35,9 +77,23 @@ export default class JobInfo extends Component {
       return <ProgressBar mode='indeterminate' />;
     }
 
+    const profile = this.props.getProfile();
+    const { jobId, status, favorited, requested, user, userId } = job;
+    const phoneNumber = user && user.phoneNumber || '';
+
     return (
       <div>
-        <JobItem job={job} />
+        <JobItem job={job}>
+          <CardActions>
+            <IconButton icon='favorite'
+              accent={favorited}
+              onClick={this.handleFavorite.bind(this, !favorited)} />
+            <IconButton icon='call' onClick={this.handleShowPhoneNumber.bind(this, phoneNumber)} />
+            <IconButton icon='add'
+              disabled={requested || profile.userId === userId}
+              onClick={this.handleRequestJob.bind(this)} />
+            </CardActions>
+        </JobItem>
       </div>
     )
   }
