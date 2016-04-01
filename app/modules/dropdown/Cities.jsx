4e7a41c11bc2fa94fee61e05ba95845e72b3868c
Lupino <lmjubuntu@gmail.com>
2016-04-01 16:35:07 +0800

    update Cities component

--- app/modules/dropdown/Cities.jsx
+++ app/modules/dropdown/Cities.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import { Dropdown } from 'react-toolbox';
+import { getCities } from '../../api';
 
 export default class Cities extends Component {
 
+  state = {
+    source: []
+  };
+
+  loadCities() {
+    const { type } = this.props;
+    getCities((err, rsp) => {
+      if (err) {
+        alert(err);
+        return;
+      }
+      const { cities } = rsp;
+      const source = cities.map(({ cityId, cityName }) => {
+        return { value: cityId, label: cityName }
+      });
+      this.setState({ source });
+    });
+  }
+
+  componentDidMount() {
+    this.loadCities();
+  }
+
   render() {
     const { ...props } = this.props;
-
-    const source = [
-      { value: 'xiamen', label: '厦门' },
-      { value: 'zhangzhou', label: '漳州' },
-      { value: 'quanzhou', label: '泉州' }
-    ];
-
+    const { source } = this.state;
     return (
       <Dropdown
         source={source}
