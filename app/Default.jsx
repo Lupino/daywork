54251af389f1a60e2b7c9ffbfdd4da36655c89b2
Lupino <lmjubuntu@gmail.com>
2016-04-03 17:05:52 +0800

    apply search result on default page

--- app/Default.jsx
+++ app/Default.jsx
@@ -5,8 +5,12 @@ import {
 } from 'react-toolbox';
 
 import JobItem from './jobs/JobItem';
+import ServiceItem from './services/ServiceItem';
 import lodash from 'lodash';
-import { getJobs, requestJob, favorite, unfavorite } from './api';
+import {
+  search, requestJob, favorite, unfavorite,
+  favoriteService, unfavoriteService
+} from './api';
 import style from './style';
 
 export default class Default extends Component {
@@ -16,26 +20,24 @@ export default class Default extends Component {
 
   state = {
     loadMoreButton: true,
-    currentPage: 0,
-    jobs: []
+    docs: [],
+    q: 'status:Publish status:Finish',
+    total: 0,
+    from: 0,
+    size: 10
   };
 
-  handleLoadJobs(page) {
-    page = page || 0;
-    const limit = 10;
-    const status = 'Publish';
+  handleSearch(from) {
+    from = from || 0;
     const { notify } = this.props;
-    getJobs({ page, limit, status }, (err, rsp) => {
+    const { size, q } = this.state;
+    search({ from, size, q }, (err, rsp) => {
       if (err) return notify(err);
-      let { jobs } = this.state;
-      jobs = jobs.concat(lodash.clone(rsp.jobs));
-      jobs = lodash.uniq(jobs, 'jobId');
-      this.setState({ jobs, currentPage: page });
-      if (rsp.jobs.length < limit) {
-        this.setState( { loadMoreButton: false } );
-      } else {
-        this.setState( { loadMoreButton: true } );
-      }
+      let { docs } = this.state;
+      docs = docs.concat(lodash.clone(rsp.docs));
+      const { total, size, from, q } = rsp;
+      const loadMoreButton = total > from + size;
+      this.setState({ docs, total, size, from, q, loadMoreButton } );
     });
   }
 
@@ -47,6 +49,15 @@ export default class Default extends Component {
       router.push(`/works/${jobId}`);
     } else {
       router.push(`/job_info/${jobId}`);
+    }
+  }
+
+  handleShowService(serviceId, { isOwner }) {
+    const { router } = this.context;
+    if (isOwner) {
+      router.push(`/services/${serviceId}`);
+    } else {
+      router.push(`/service_info/${serviceId}`);
     }
   }
 
@@ -80,27 +91,41 @@ export default class Default extends Component {
     const { notify } = this.props;
     let method = fav ? favorite : unfavorite;
 
-    this.updateFavoriteButton(jobId, fav);
+    this.updateFavoriteButton({ jobId }, fav);
     method({ jobId }, (err) => {
       if (err) {
-        this.updateFavoriteButton(jobId, !fav);
+        this.updateFavoriteButton({ jobId }, !fav);
         return notify(err);
       }
     });
   }
 
-  updateFavoriteButton(jobId, favorited) {
-    let jobs = this.state.jobs.map(job => {
-      if (job.jobId === jobId) {
-        job.favorited = favorited;
+  handleFavoriteService(serviceId, fav) {
+    const { notify } = this.props;
+    let method = fav ? favoriteService : unfavoriteService;
+
+    this.updateFavoriteButton({ serviceId }, fav);
+    method({ serviceId }, (err) => {
+      if (err) {
+        this.updateFavoriteButton({ serviceId }, !fav);
+        return notify(err);
       }
-      return job;
     });
-    this.setState({ jobs });
+  }
+
+
+  updateFavoriteButton({ jobId, serviceId }, favorited) {
+    let docs = this.state.docs.map(doc => {
+      if (doc.jobId === jobId || doc.serviceId === serviceId) {
+        doc.favorited = favorited;
+      }
+      return doc;
+    });
+    this.setState({ docs });
   }
 
   componentDidMount() {
-    this.handleLoadJobs();
+    this.handleSearch();
   }
 
   renderJob(job) {
@@ -122,20 +147,41 @@ export default class Default extends Component {
     );
   }
 
+  renderService(service) {
+    const { serviceId, status, favorited, user, userId, isOwner } = service;
+    const phoneNumber = user && user.phoneNumber || '';
+    return (
+      <ServiceItem service={service} key={`service-${serviceId}`}>
+        <CardActions>
+          <IconButton icon='favorite'
+            accent={favorited}
+            onClick={this.handleFavorite.bind(this, serviceId, !favorited)} />
+          <IconButton icon='remove_red_eye' raised onClick={this.handleShowService.bind(this, serviceId, { isOwner })} />
+          <IconButton icon='call' onClick={this.handleShowPhoneNumber.bind(this, phoneNumber)} />
+        </CardActions>
+      </ServiceItem>
+    );
+  }
+
   render() {
-    const jobs = this.state.jobs.map((job) => this.renderJob(job));
-    const { loadMoreButton, currentPage } = this.state;
+    const docs = this.state.docs.map((doc) => {
+      if (doc.jobId) {
+        return this.renderJob(doc);
+      }
+      return this.renderService(doc);
+    });
+    const { loadMoreButton, from, size } = this.state;
 
     return (
       <div>
-        {jobs}
+        {docs}
         { loadMoreButton &&
           <Button
             label='加载更多...'
             raised
             primary
             className={style['load-more']}
-            onClick={this.handleLoadJobs.bind(this, currentPage + 1)}
+            onClick={this.handleSearch.bind(this, from + size)}
           />
         }
       </div>
