ba76a42db159c0caf9e82a8aeb14641baa3ce13a
Lupino <lmjubuntu@gmail.com>
2016-05-01 22:17:08 +0800

    show user info on worker

--- app/jobs/UserItem.jsx
+++ app/jobs/UserItem.jsx
@@ -6,15 +6,20 @@ import style from '../style';
 import { imageRoot } from '../api';
 
 export default class UserItem extends Component {
+  handleShowUserInfo(userId) {
+    const { router } = this.context;
+    router.push(`/user_info/${userId}`);
+  }
   render() {
     const { worker, children } = this.props;
-    const { realName, avatar, phoneNumber, sex } = worker.user;
+    const { realName, avatar, phoneNumber, sex, userId } = worker.user;
     return (
       <Card className={style.card}>
         <CardTitle
           avatar={avatar && avatar.key ? `${imageRoot}${avatar.key}` : '/static/default-avatar.png'}
           title={realName}
           subtitle={phoneNumber}
+          onClick={this.handleShowUserInfo.bind(this, userId)}
         />
         <CardText>{`性别 ${sex === 'M'? '男' : '女'}`}</CardText>
         {children}
@@ -30,4 +35,8 @@ UserItem.propTypes = {
 
 UserItem.defaultProps = {
   worker: {}
+}
+
+UserItem.contextTypes = {
+  router: PropTypes.object
 }
