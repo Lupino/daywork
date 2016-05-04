557d317712ae0f6c539b99d80d3a2edce513c442
Lupino <lmjubuntu@gmail.com>
2016-05-04 13:34:40 +0800

    parse search json query

--- app/api/search.js
+++ app/api/search.js
@@ -2,5 +2,8 @@ import request from './request';
 import { getUri, wapperCallback } from './utils';
 
 export function search({ q, from, size }, callback) {
+  if (typeof q !== 'string') {
+    q = JSON.stringify(q);
+  }
   request.get(getUri('/api/search/', { q, from, size }), wapperCallback(callback));
 }
